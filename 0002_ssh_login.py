14584a18dc29878aafbe716cd502ef153864bdbf
Gopikrishna <a.gopikrishna01@gmail.com>
2017-11-18 18:47:10 +0530

    Create 0002_ssh_login.py

---
+++ 0002_ssh_login.py
@@ -0,0 +1,30 @@
+
+import paramiko
+
+class PdpPerfomance(object):
+	"""docstring for ClassName"""
+	def __init__(self):
+		self.ip = '10.0.0.0'
+		self.port = 22
+		self.username = 'user'
+		self.password = 'password'
+
+	def ssh_login(self):
+
+		try:
+			ssh=paramiko.SSHClient()
+			ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+			print("connecting...")
+			ssh.connect(self.ip,self.port,self.username,self.password)
+			print("connected!!!")
+			stdin, stdout, stderr = ssh.exec_command('ls')
+			print stdout.readlines()
+			ssh.close()
+			print("Connection closed!!!")
+		except:
+			print("Connection Failed!!!")
+
+
+
+obj = PdpPerfomance()
+obj.ssh_login()
