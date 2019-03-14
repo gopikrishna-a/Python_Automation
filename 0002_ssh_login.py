de21f4d81eb0a3b5b506c48d230e13fd906a1bad
Gopikrishna Arepalli <a.gopikrishna01@gmail.com>
2019-03-14 19:49:11 +0530

    Update 0002_ssh_login.py

--- 0002_ssh_login.py
+++ 0002_ssh_login.py
@@ -1,30 +1,28 @@
-
 import paramiko
 
-class SshLogin(object):
-	"""docstring for ClassName"""
-	def __init__(self):
-		self.ip = '10.0.0.0'
-		self.port = 22
-		self.username = 'user'
-		self.password = 'password'
+class RemoteLogin(object):
+    """docstring for ClassName"""
+    def __init__(self):
+        self.ip = '10.0.0.0'
+        self.port = 22
+        self.username = 'user'
+        self.password = 'password'
 
-	def ssh_login(self):
+    def ssh_login(self):
 
-		try:
-			ssh=paramiko.SSHClient()
-			ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-			print("connecting...")
-			ssh.connect(self.ip,self.port,self.username,self.password)
-			print("connected!!!")
-			stdin, stdout, stderr = ssh.exec_command('ls')
-			print stdout.readlines()
-			ssh.close()
-			print("Connection closed!!!")
-		except:
-			print("Connection Failed!!!")
+        try:
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            print("connecting...")
+            ssh.connect(self.ip,self.port,self.username,self.password)
+            print("connected!!!")
+            stdin, stdout, stderr = ssh.exec_command('ls')
+            print stdout.readlines()
+            ssh.close()
+            print("Connection closed!!!")
+        except Exception as err:
+            print("Connection Failed!!! {}".format(err))
 
 
-
-obj = SshLogin()
+obj = RemoteLogin()
 obj.ssh_login()
